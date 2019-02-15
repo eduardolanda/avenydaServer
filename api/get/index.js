8e917ef5cb8bad8d26f557642d39b423d5886bdf
Eduardo Landa <eduardo.landa@outlook.com>
2019-02-15 14:38:28 -0800

    first commit

---
+++ api/get/index.js
@@ -0,0 +1,23 @@
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const pool = require('../../DB/db')
+
+const app = new Koa()
+app.use(bodyParser())
+
+app.use(async ctx => {
+  // const dbtitle = await ctx.request.body.title
+  const item = await show()
+  ctx.body = item
+})
+
+async function show() {
+  try {
+    const itemData = await pool.query(`SELECT * FROM avenyda.User WHERE id=1`)
+    return itemData
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = app.callback()
